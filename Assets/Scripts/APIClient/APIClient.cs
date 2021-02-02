46960079583300e7c63240f071006e1690a58a05
Alan P John <alanpjohn@outlook.com>
2021-02-02 09:50:42 +0530

    Setup API Client models and singleton
    Singleton class and models for requests and responses made

---
+++ Assets/Scripts/APIClient/APIClient.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using APIClient.Models;
+using APIClient.Singleton;
+
+namespace APIClient
+{
+
+    public class APIClient : Singleton<APIClient>
+    {
+        private string serverEndpoint = "https://argorithm.el.r.appspot.com";
+
+        // public IEnumerator connect(string url, System.Action<ConnectionResponse> callback){}
+
+        // public IEnumerator create(Account user,System.Action<CreationResponse> callback){}
+
+        // public IEnumerator login(Account user,System.Action<AuthResponse> callback){}
+
+        // public IEnumerator list(System.Action<List<ARgorithm>> callback){}
+
+    }
+
+}
---
+++ Assets/Scripts/APIClient/Models/Models.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace APIClient.Models{
+    
+    public class ConnectionResponse{
+        public string status;
+    }
+
+    public class Account{
+        public string email;
+        public string password;
+    }
+
+    public class CreationResponse{
+        public string status;
+    }
+
+    public class LoginResponse{
+        public string status;
+        public string token;
+    }
+
+    public class ARgorithm{
+        public string argorithmID;
+        public string maintainer;
+        public string description;
+    }
+
+}
