ce4c0a3fd38598bcd42f4802c8acac7d9c630880
Yatharth <yatharth.mathur@gmail.com>
2021-02-23 14:18:00 +0530

    added Engine namespace

---
+++ Assets/Scripts/ARgorithm/Engine/Engine.cs
@@ -0,0 +1,16 @@
+﻿using ARgorithm.Utils;
+
+namespace ARgorithm.Engine
+{
+    public class Engine{
+    
+    }
+
+    public class Stage{
+
+
+        public Stage(StageData stageData) {
+            
+        }
+    }
+}
